a1a69761723dd6207ff9b8ad4efd02ec1f8ed8fe
CClemensJr <cclemensjr@gmail.com>
2019-02-04 13:32:57 -0800

    Added navigation properties for all classes

--- ColorWheelAPI/ColorWheelAPI/Models/Colors.cs
+++ ColorWheelAPI/ColorWheelAPI/Models/Colors.cs
@@ -18,5 +18,14 @@ namespace ColorWheelAPI.Models
         public string ColorName { get; set; }
         public string HexCode { get; set; }
 
+        /// <summary>
+        /// Navigation Properties
+        /// </summary>
+        public Analogous Analogous { get; set; }
+        public Complimentary Complimentary { get; set; }
+        public Monochromatic Monochromatic { get; set; }
+        public SplitComplimentary SplitComplimentary { get; set; }
+        public Tetradic Tetradic { get; set; }
+        public Triadic Triadic { get; set; }
     }
 }

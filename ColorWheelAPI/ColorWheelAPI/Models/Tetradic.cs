9c5c8e20c710a9828410c4da1086add14e01f6ba
castillocarlosr <castillocarlosr@gmail.com>
2019-02-04 13:27:42 -0800

    adding models except color

--- ColorWheelAPI/ColorWheelAPI/Models/Tetradic.cs
+++ ColorWheelAPI/ColorWheelAPI/Models/Tetradic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,25 @@ namespace ColorWheelAPI.Models
 {
     public class Tetradic
     {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Foreign Keys
+        /// </summary>
+        [ForeignKey("ColorOneID")]
+        public int ColorOneID { get; set; }
+
+        [ForeignKey("ColorTwoID")]
+        public int ColorTwoID { get; set; }
+
+        [ForeignKey("ColorThreeID")]
+        public int ColorThreeID { get; set; }
+
+        [ForeignKey("ColorFourID")]
+        public int ColorFourID { get; set; }
+
+
+        //Navigation
+        public Colors color { get; set; }
     }
 }
--- ColorWheelAPI/ColorWheelAPI/Models/Triadic.cs
+++ ColorWheelAPI/ColorWheelAPI/Models/Triadic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,22 @@ namespace ColorWheelAPI.Models
 {
     public class Triadic
     {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Foreign Keys
+        /// </summary>
+        [ForeignKey("ColorOneID")]
+        public int ColorOneID { get; set; }
+
+        [ForeignKey("ColorTwoID")]
+        public int ColorTwoID { get; set; }
+
+        [ForeignKey("ColorThreeID")]
+        public int ColorThreeID { get; set; }
+      
+
+        //Navigation
+        public Colors color { get; set; }
     }
 }

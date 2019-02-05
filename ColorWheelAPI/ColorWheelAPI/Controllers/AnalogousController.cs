30b5c9e3e01555499c2db98789601bf3f33f0f0c
CClemensJr <cclemensjr@gmail.com>
2019-02-05 11:03:02 -0800

    Commented out get action for check route

--- ColorWheelAPI/ColorWheelAPI/Controllers/AnalogousController.cs
+++ ColorWheelAPI/ColorWheelAPI/Controllers/AnalogousController.cs
@@ -40,10 +40,25 @@ namespace ColorWheelAPI.Controllers
         }
 
 
-        [HttpGet("{id},{id},{id}")]
-        public IActionResult Get(string id1, string id2, string id3)
-        {
+        //[HttpGet("{id},{id},{id}")]
+        //public IActionResult Get(string id1, string id2, string id3)
+        //{
+            // Bring in context
+            //bool temp = false;
+            //Analogous analogous = _context.Analogous.FirstOrDefault(a => 
+            //{     
+            //    for (int i = 0; i < 12; i++)
+            //    {
+            //        if(a.ColorOneID. == id1)
+            //    }
+            //});
 
+
+            //return Ok(false);
+                // If id1 has a palette, compare other palette colors to id2 and id3
+            // Check for palettes that have all three id colors
+            // If palette is found, return true;
+            // If not, return false;
         }
 
     }
